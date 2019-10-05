92999ee6c0ead8d8cf45e4826192854da40e8164
Vlad Zaporozhskyi <zapovlad@gmail.com>
2019-10-04 22:19:24 -0400

    Update coinManager.swift

--- ByteCoin/Model/coinManager.swift
+++ ByteCoin/Model/coinManager.swift
@@ -11,7 +11,7 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","UAH","USD","ZAR"]
     
     
     
